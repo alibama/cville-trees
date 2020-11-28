aba03e05e7d409dbf058810b3b0ccde368b7cb55
alibama <adp6j@virginia.edu>
2020-11-27 22:05:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-import numpy as np
-import fiona
-import geopandas as gpd
-import requests
-import datetime
-import json, re
+altair
+pandas
+streamlit
+shapely
+fiona
+geopandas
