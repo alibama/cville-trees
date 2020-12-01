c69f02b4c700b65e02f77d40a94526babf719f53
alibama <adp6j@virginia.edu>
2020-11-30 21:46:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ trees=gpd.read_file("https://opendata.arcgis.com/datasets/e7c856379492408e9543a2
 zip_url = 'http://widget.charlottesville.org/gis/zip_download/planning_area.zip'
 cvillehoods = gpd.read_file(zip_url)
 
-cvillehoods.head()
+trees.head()
 
 """
 <br>
