57be47cf122e5b569d0e3a86b90200683e1826c0
alibama <adp6j@virginia.edu>
2020-12-02 20:36:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,11 +44,6 @@ trees_in_hoods
 #test
 #st.map(cvillehoods)
 
-
-
-
-
-
 """test date time widgets"""
 
 layers = [
@@ -59,37 +54,11 @@ layers = [
     ),
 
 ]
-pdk.Deck(layers, map_provider=None).to_html("geopandas_integration.html", css_background_color="cornflowerblue")
-
-
-UK_ACCIDENTS_DATA = (
-    "https://raw.githubusercontent.com/uber-common/"
-    "deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"
-)
-
-
-@st.cache
-def get_data():
-    return pd.read_csv(UK_ACCIDENTS_DATA)[["lng", "lat"]]
-
-
-data = get_data().dropna() # IMPORTANT TO DROP NA
-
-layer = pdk.Layer(
-    "HexagonLayer",
-    data=data,
-    get_position="[lng, lat]",
-    auto_highlight=True,
-    elevation_scale=50,
-    pickable=True,
-    elevation_range=[0, 3000],
-    extruded=True,
-    coverage=1,
-)
+#pdk.Deck(layers, map_provider=None).to_html("geopandas_integration.html", css_background_color="cornflowerblue")
 
 # Set the viewport location
 view_state = pdk.ViewState(
-    longitude=-1.415, latitude=52.2323, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36
+    longitude=-38, latitude=78.2323, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36
 )
 
 # Combined all of it and render a viewport
@@ -97,9 +66,15 @@ r = pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
     layers=[layer],
     initial_view_state=view_state,
-    tooltip={"html": "<b>Elevation Value:</b> {elevationValue}", "style": {"color": "white"}},
 )
 st.pydeck_chart(r)
+
+
+
+
+
+
+
 
 
 today = datetime.date.today()
