eee85e2a43d18e12b3424a3dd657994952d6a579
alibama <adp6j@virginia.edu>
2020-12-11 14:51:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,11 @@ layer = [
         getFillColor=[60, 220, 255],
         getRadius=dotradius, #here's the streamlit slider widget being used to determine the size of the point on the deckgl map
     ),
-#    pdk.Layer(
-#        "GeoJsonLayer",
-#        data=cvillegeo,
-#        getFillColor=[20, 20, 123],
-#    ),
+    pdk.Layer(
+        "GeoJsonLayer",
+        data=cvillegeo,
+        getFillColor=[20, 20, 123],
+    ),
 ]
 
 
