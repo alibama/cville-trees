5ab9a4ed2933f2708691db8537eaa108e81a65ce
alibama <adp6j@virginia.edu>
2020-12-16 22:20:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import pydeck as pdk
 import pyproj
 from shapely.ops import orient # https://gis.stackexchange.com/questions/336477/how-to-apply-the-orient-function-on-the-geometry-of-a-geopandas-dataframe
 import osgeo
-from osgeo import gdal
+#from osgeo import gdal
 #import gdal
 import sys
 sys.path
@@ -37,8 +37,9 @@ trees=gpd.read_file("https://opendata.arcgis.com/datasets/e7c856379492408e9543a2
 zip_url = "http://widget.charlottesville.org/gis/zip_download/planning_area.zip"
 cvillehoods = gpd.read_file(zip_url)
 #cvillehoods.crs="EPSG:4326" # https://geopandas.org/projections.html 
-cvillehoods.geometry = cvillehoods.geometry.apply(orient, args=(-1,)) #fix the right hand rule orientation issues 
-cvillehoods=cvillehoods[['NAME', 'geometry']]
+cvillecrs =cvillehoods.to_crs(epsg=4326, inplace=True)
+#cvillehoods.geometry = cvillehoods.geometry.apply(orient, args=(-1,)) #fix the right hand rule orientation issues 
+cvillecrs=cvillecrs[['NAME', 'geometry']]
 cvillehoods.to_file("cvillehoods.geojson", driver='GeoJSON')
 cvillegeo=gpd.read_file("cvillehoods.geojson")
 
