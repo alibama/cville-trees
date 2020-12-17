a112eb57938465c1daa275861edcef6f043c20e8
alibama <adp6j@virginia.edu>
2020-12-16 20:17:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ This is a quick look at finding publicly managed trees in cville using the https
 trees=gpd.read_file("https://opendata.arcgis.com/datasets/e7c856379492408e9543a25d684b8311_79.geojson")
 zip_url = "http://widget.charlottesville.org/gis/zip_download/planning_area.zip"
 cvillehoods = gpd.read_file(zip_url)
-cvillehoods.crs="EPSG:4326" # https://geopandas.org/projections.html 
+#cvillehoods.crs="EPSG:4326" # https://geopandas.org/projections.html 
 cvillehoods.geometry = cvillehoods.geometry.apply(orient, args=(-1,)) #fix the right hand rule orientation issues 
 cvillehoods=cvillehoods[['NAME', 'geometry']]
 cvillehoods.to_file("cvillehoods.geojson", driver='GeoJSON')
