ecb36bf391489c581b24ccf92101c53860c38982
alibama <adp6j@virginia.edu>
2020-12-18 21:57:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ trees=gpd.read_file("https://opendata.arcgis.com/datasets/e7c856379492408e9543a2
 api = overpass.API()
 cvilleresult = api.get('way["place"="neighbourhood"](37.964522,-78.573741,38.097572,-78.415126);', responseformat="geojson", verbosity="geom")
 #cvillefile = io.StringIO(cvilleresult)
-type(cvilleresult)
+st.write(type(cvilleresult))
 #cvillegeo=gpd.read_file(cvillefile)
 """
 Testing geopandas & libspatialindex
