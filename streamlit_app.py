c6ab47112ae17eb11774ea71bda77211a0b39e61
alibama <adp6j@virginia.edu>
2020-12-20 17:27:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,16 +53,12 @@ cvillehoods = gpd.read_file("https://opendata.arcgis.com/datasets/c371ad0b810248
 #cvillegeo = gpd.read_file(api.get('way["place"="neighbourhood"](37.964522,-78.573741,38.097572,-78.415126);', responseformat="geojson", verbosity="geom"))
 #https://gis.stackexchange.com/questions/130963/write-geojson-into-a-geojson-file-with-python
 
-
-
-
-
 #cvillegdf=gpd.read_file(cvilleresult)
 """
 Testing geopandas & libspatialindex
 """
 trees_in_hoods=gpd.sjoin(trees, cvillehoods, how='left', op='contains')
-trees_in_hoods.head()
+trees_in_hoods
 treetype = sorted(trees['Common_Name'].drop_duplicates()) # select all of the trees from the dataframe and filter by unique values and sorted alphabetically to create a useful dropdown menu list
 tree_choice = st.sidebar.selectbox('Tree type:', treetype) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
 trees=trees[trees['Common_Name'].str.contains(tree_choice)] # create a dataframe for our deck.gl map to use in the layer as the data source and update it based on the selection made above
