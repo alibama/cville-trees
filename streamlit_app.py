858bf09bbe70890a454f6f795292636490c1d3e7
alibama <adp6j@virginia.edu>
2020-12-20 21:06:12 -0500

    Update streamlit_app.py
    wrapping up a working draft that includes some spatial joins and other stuff.... still needs a choropleth map

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,17 +19,18 @@ import geojson
 # Welcome to The Cville Tree Commission Neighborhood Tree App!
 As part of the Charlottesville Tree Commission we're working to help the city of Charlottesville make greater value of the available local, regional, and national datasets in understanding tree ecosystems and canopy as it relates to public health and more equitable living experiences in our shared community.
 
-One of the best local data sets is Charlottesville's own open data portal available here https://opendata.charlottesville.org/pages/download-gis-data-shp-cad
+One of the best local data sets is Charlottesville's own open data portal available here https://opendata.charlottesville.org
+
 From this dataset hosted in ArcEsri's online data portal we can find files in CSV, geoJSON, .shp and many other standard geospatial data formats
 For this example we will be using the neighborhood data to begin better understanding our community actions.
-
+https://opendata.charlottesville.org/datasets/planning-neighborhood-area (thanks for spotting this @https://twitter.com/jalbertbowdenii for tracking this down 
 
 Another local ArcEsri database hosted at the University of Virginia is UVa's Equity Atlas https://equity-atlas-uvalibrary.opendata.arcgis.com/datasets/charlottesville::tree-inventory-point and tools like https://opendata.charlottesville.org/pages/download-gis-data-shp-cad
 For this example we are pulling our tree point data (specifically only the publicly owned trees on city property... more on that later) to begin our analysis
 
-https://gis.stackexchange.com/questions/225586/reading-raw-data-into-geopandas is a quick tutorial for getting data in to https://geopandas.org/ 
+https://gis.stackexchange.com/questions/225586/reading-raw-data-into-geopandas is a tutorial for getting data in to https://geopandas.org/ 
 
-This is a quick look at finding publicly managed trees in cville using the https://deck.gl/ library & https://www.streamlit.io/
+This is a look at finding publicly managed trees in cville using the https://deck.gl/ library & https://www.streamlit.io/
 
 """
 trees=gpd.read_file("https://opendata.arcgis.com/datasets/e7c856379492408e9543a25d684b8311_79.geojson")
@@ -44,22 +45,25 @@ cvillehoods = gpd.read_file("https://opendata.arcgis.com/datasets/c371ad0b810248
 #cvillegeo = gpd.read_file(api.get('way["place"="neighbourhood"](37.964522,-78.573741,38.097572,-78.415126);', responseformat="geojson", verbosity="geom"))
 #https://gis.stackexchange.com/questions/130963/write-geojson-into-a-geojson-file-with-python
 
-#cvillegdf=gpd.read_file(cvilleresult)
 """
-Testing geopandas & libspatialindex?
+Testing geopandas sjoin - assigns a neighborhood to each tree point
 """
 trees_in_hoods=gpd.sjoin(trees, cvillehoods, how='left', op='intersects')
 
+"""
+Neighborhood totaals are counted
+"""
+
 st.write(trees_in_hoods['NAME'].value_counts())
 
-#st.write(list(trees_in_hoods.columns))
+
 treetype = sorted(trees['Common_Name'].drop_duplicates()) # select all of the trees from the dataframe and filter by unique values and sorted alphabetically to create a useful dropdown menu list
 tree_choice = st.sidebar.selectbox('Tree type:', treetype) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
 trees=trees[trees['Common_Name'].str.contains(tree_choice)] # create a dataframe for our deck.gl map to use in the layer as the data source and update it based on the selection made above
 
 dotradius = st.sidebar.slider("Tree dot radius",1,100,50,1) # this creates a slider widget called "tree dot radius" with the format of "slider name", followed by the minimum value, the maximum value, the default value, and the incremental movement value
 """
-this should work. #https://gis.stackexchange.com/questions/255586/gdal-vectortranslate-returns-empty-object
+And then we write the deck.gl layers from the geopandas dataframes using streamlit.io widgets to edit the data on the screen
 """
 
 layer = [
@@ -92,6 +96,10 @@ r = pdk.Deck(
     tooltip={"html": "<b>Tree species:</b> {tree_choice}", "style": {"color": "white"}},
 )
 st.pydeck_chart(r)
+
+"""
+And lastly we provide some geojson output files in case someone wants to check our work in some other system
+"""
 
 import os
 import base64
