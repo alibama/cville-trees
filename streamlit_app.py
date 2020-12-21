b647844aebbad7343845946a601d1766c8c84388
alibama <adp6j@virginia.edu>
2020-12-20 20:40:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,8 @@ Testing geopandas & libspatialindex?
 """
 trees_in_hoods=gpd.sjoin(trees, cvillehoods, how='left', op='intersects')
 st.write(trees_in_hoods.head())
+
+st.write(list(trees_in_hoods.columns))
 treetype = sorted(trees['Common_Name'].drop_duplicates()) # select all of the trees from the dataframe and filter by unique values and sorted alphabetically to create a useful dropdown menu list
 tree_choice = st.sidebar.selectbox('Tree type:', treetype) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
 trees=trees[trees['Common_Name'].str.contains(tree_choice)] # create a dataframe for our deck.gl map to use in the layer as the data source and update it based on the selection made above
